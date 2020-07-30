e35e339925897a3e922f1db61101b2856c1360a3
Jamie Schrifrin <jamieschifrin@hotmail.com>
2020-07-30 09:07:21 -0400

    first update

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, {Component}  from 'react';
+import Ninjas from './ninjas'
+import AddNinja from'./addNinja'
+
+
+class App extends Component {
+  state ={
+    ninjas : [
+      {name: 'ryu', age: 30, belt: 'black', id:1},
+      {name: 'Yoshi', age: 20, belt: 'green', id:2},
+      {name: 'Crystal', age: 25, belt: 'pink', id:3},
+    ]
+  }
+   render() {
+     return (
+       <div className="App">
+      <hi>My First React App</hi>
+      <p>Welcome :)</p>
+      <Ninjas ninjas={this.state.ninjas}/>
+      <AddNinja />
+      </div>
+     )
+     }
+  }
+
+  export default App
