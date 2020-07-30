0221a9cf3a925501dd3bf752324c63edcf805763
Jamie Schrifrin <jamieschifrin@hotmail.com>
2020-07-30 09:36:47 -0400

    Updates

---
+++ precache-manifest.012669992ca46836bb3c1b1872904d22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5088f2bb032a7c9dc5281d561fc59c4",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "8e22f99a094dcf8eea87",
+    "url": "/my-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6374b44df704401b3581",
+    "url": "/my-app/static/js/2.b867d86b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.b867d86b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e22f99a094dcf8eea87",
+    "url": "/my-app/static/js/main.dcb0d696.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  }
+]);
