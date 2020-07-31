4060ecd2f8ab9c4232807e0270fd69cb456fa343
Jamie Schrifrin <jamieschifrin@hotmail.com>
2020-07-31 08:11:36 -0400

    hw

--- src/App.js
+++ src/App.js
@@ -11,13 +11,36 @@ class App extends Component {
       {name: 'Crystal', age: 25, belt: 'pink', id:3},
     ]
   }
+  addNinja = (ninja) => {
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja]
+    this.setState({
+      ninjas: ninjas
+    })
+    deleteNinja = (id) => { 
+      let ninjas = this.state.ninjas.filter(ninja => {
+        return ninja.id != id
+      });
+      this.setState({
+        ninjas :ninja
+      })
+}
+  }
+  componentDidMount(){
+    console.log('component mounted')
+  this.componentDidUpdate(prevProps, prevState){
+    console.log('component updated');
+    
+  }
+  }
    render() {
      return (
        <div className="App">
       <hi>My First React App</hi>
       <p>Welcome :)</p>
-      <Ninjas ninjas={this.state.ninjas}/>
-      <AddNinja />
+      <Ninjas deleteNinja={this.deleteNinja}   ninjas={this.state.ninjas}/>
+      <AddNinja addNinja={this.addNinja} />
+
       </div>
      )
      }
--- src/ninjas.js
+++ src/ninjas.js
@@ -1,26 +1,14 @@
 import React  from 'react';
+import './Ninjas.css';
 
-const Ninjas = ({ninjas}) =>{ 
-           
-            // const ninjaList = ninjas.map(ninja => {
-            //     if(ninja.age > 20){
-            //     return (
-            //       <div className="ninja" key={ninja.id}>
-            //         <div>Name: {ninja.name}</div>
-            //         <div>Age: {ninja.age}</div>
-            //         <div>Belt: {ninja.belt}</div>
-            //       </div>
-            //     )
-            //     }else{
-            //         return null;
-            //     }
-            // })
+const Ninjas = ({ninjas, deleteNinja}) =>{ 
             const ninjaList = ninjas.map(ninja => {
               return ninjas.age > 20 ? (
                 <div className="ninja" key={ninja.id}>
                  <div>Name: {ninja.name}</div>
                  <div>Age: {ninja.age}</div>
                  <div>Belt: {ninja.belt}</div>
+                 <button onClick={() => {deleteNinja(ninja.id)}}>Delete Ninja</button>
                 </div>
 
 
